b773594cca7413dd54273a5e97142098559324f9
ecmatonix <ecmatonix@gmail.com>
2019-04-11 20:24:34 +0300

    Delete simple logging for the service worker

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 var CACHE_NAME_PREFIX = "swl-app-cache";
-var CACHE_VERSION = "0.1.18";
+var CACHE_VERSION = "0.1.19";
 var CACHE_NAME = CACHE_NAME_PREFIX + "-" + CACHE_VERSION;
 var urlsToCache = [
   "/",
@@ -28,19 +28,14 @@ var urlsToCache = [
 ];
 
 self.addEventListener("install", function(event) {
-  console.log("Install handler (service worker):");
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-      console.log("\tOpened cache: " + CACHE_NAME);
-      console.log("\tAdd to cache:");
-      console.dir(urlsToCache);
       return cache.addAll(urlsToCache);
     })
   );
 });
 
 self.addEventListener("activate", function(event) {
-  console.log("Activate handler (service worker):");
   var cacheNamesList = [CACHE_NAME];
 
   event.waitUntil(
@@ -48,7 +43,6 @@ self.addEventListener("activate", function(event) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
           if (cacheNamesList.indexOf(cacheName) === -1) {
-            console.log("\tDelete cache: " + cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -58,19 +52,14 @@ self.addEventListener("activate", function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
-  console.log("Fetch handler (service worker):");
-  console.log("\tFetch request: " + event.request.url);
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      console.log("\tSearch in cache: " + event.request.url);
       return response ? response : update(event.request);
     })
   );
 });
 
 function update(request) {
-  console.log("\tupdate function (service worker):");
-  console.log("\t\tRequest: " + request.url);
   var fetchRequest = request.clone();
 
   return fetch(fetchRequest).then(function(response) {
@@ -79,19 +68,15 @@ function update(request) {
 }
 
 function checkResponse(response) {
-  console.log("\tcheckResponse function (service worker):");
   return (
     !response || response.status !== 200 || /browser-sync/.test(response.url)
   );
 }
 
 function addToCache(request, response) {
-  console.log("\taddToCache function (service worker):");
   var responseToCache = response.clone();
 
   caches.open(CACHE_NAME).then(function(cache) {
-    console.log("\t\tOpened cache: " + CACHE_NAME);
-    console.log("\t\tPut to cache: " + request.url);
     cache.put(request, responseToCache);
   });
 
